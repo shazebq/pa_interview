23137027a38b1e46bcb18ae5f9821066a815536c
Shazeb Qadir <sqadir@twitter.com>
2019-07-14 18:24:54 -0700

    updates

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,10 +1,15 @@
 class TodosController < ApplicationController
 
-  def index
-    # render(json: {message: 'hello world'}, :status => :ok)
+  def create
+    # disable the button, and display "in-progress"
     ActionCable.server.broadcast "web_notifications", {
       message: '<p>hello world!</p>'
     }
+
+    # run the job
+
+
+    # after job is completed, enable button, and display "completed"
   end
 
 end
